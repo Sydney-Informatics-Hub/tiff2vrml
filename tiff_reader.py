a24da8db1ddfb309eb77423428533f47f11cb9e4
Nathaniel Butterworth <natbutter@gmail.com>
2023-11-06 09:25:31 +1100

    Updated to python code

---
+++ tiff_reader.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import numpy as np
+import tifffile as tiff
+import os
+import scipy
+import matplotlib.pyplot as plt
+
+
+# In[8]:
+
+
+# Read the tiff files, assume have sorteable filenames 001.tif, 002.tif etc.
+tiff_dir = "data/"
+tiff_files = [f for f in os.listdir(tiff_dir) if f.endswith(".tif")]
+tiff_files.sort()
+
+# Load the first TIFF file to get its dimensions
+first_tiff = tiff.imread(os.path.join(tiff_dir, tiff_files[0]))
+height, width = first_tiff.shape
+num_slices = len(tiff_files)
+
+# Create a numpy array to store the volume
+volume = np.zeros((height, width, num_slices), dtype=np.uint8)
+
+# Iterate through TIFF files and populate a volume
+for i, tiff_file in enumerate(tiff_files):
+  print(tiff_file)
+  tiff_data = tiff.imread(os.path.join(tiff_dir, tiff_file))
+  volume[:, :, i] = tiff_data
+
+
+# In[9]:
+
+
+for i in range(volume.shape[2]):
+    slice_2d = volume[:, :, i]
+    plt.imshow(slice_2d, cmap='viridis')
+    plt.title(f"Slice {i}")
+    plt.colorbar()
+    plt.show()
+
+
+# In[ ]:
+
+
+
+
